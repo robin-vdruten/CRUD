70a92c921b1a9c9364a2ba53a3b7919646836501
liam-kilsdonk <94444182+liam-kilsdonk@users.noreply.github.com>
2022-05-13 09:22:59 +0200

    begin header
    nog met kleuren aan het kijken

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="../css/styles.css" />
+
+<header>
+  <div class="up-head">
+    <img src="../images/Artboard 1.png" height="140px" alt="SunRise" />
+    <div class="center">
+      <nav>
+        <input type="text" placeholder="Search.." />
+        <button><a href="">Over Ons</a></button>
+        <button><a href="">Informatie</a></button>
+        <button><a href="">Contact</a></button>
+      </nav>
+    </div>
+  </div>
+  <div class="down-head">
+      
+  </div>
+</header>
