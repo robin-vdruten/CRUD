bdd11a8ffc851f564a85ff49ed750dbed26ccd8e
robin-vdruten <1196364@student.roc-nijmegen.nl>
2022-05-13 09:22:30 +0200

    begin

---
+++ includes/footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/styles.css" />
+    <script src="https://kit.fontawesome.com/143048b42c.js"></script>
+  </head>
+  <body>
+    <section class="footer">
+        <div class="footer-main">
+          <div class="footer-wrapper">
+              <div class="content">
+                  <div class="box">
+                      <h5>Contact Sunrise</h5>
+                      Heb je een vraag contact ons hier!
+                      <a href="#">Contact</a>
+                  </div>
+                  <div class="box">
+                      <h5>Bestemingen</h5>
+                      <div class="tekst">
+                          <ul>
+                              <li><a href="#">Jongeren Vakantie</a></li>
+                              <li><a href="#">Jongeren Vakantie</a></li>
+                              <li><a href="#">Jongeren Vakantie</a></li>
+                          </ul>
+                      </div>
+                  </div>
+                  <div class="box">
+                      <h5>Betaal veilig en makkelijk</h5>
+                      <div class="tekst">
+                          <ul>
+                              <li><a href="#">Jongeren Vakantie</a></li>
+                              <li><a href="#">Jongeren Vakantie</a></li>
+                              <li><a href="#">Jongeren Vakantie</a></li>
+                          </ul>
+                      </div>
+                  </div>
+                  <div class="box">
+                      <h5>Stay connected</h5>
+                      <div class="tekst">
+                          <ul class="row">
+                              <li><i class="fa-brands fa-instagram"></i></li>
+                              <li><i class="fa-brands fa-youtube"></i></li>
+                              <li><i class="fa-brands fa-facebook"></i></li>
+                              <li><i class="fa-brands fa-twitter"></i></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="bottom-wrapper">
+                <div class="content">
+                    <div class="footer-nav">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
