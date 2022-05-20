8c02710306c1ea95447671d6c89e003a06ea872e
robin-vdruten <1196364@student.roc-nijmegen.nl>
2022-05-20 09:37:06 +0200

    begin login

---
+++ includes/head.php
@@ -0,0 +1,18 @@
+<?php
+function head($title)
+{
+    ?>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?php echo $title; ?></title>
+  <link rel="stylesheet" href="CSS/styles.css" />
+  <script
+    src="https://kit.fontawesome.com/143048b42c.js"
+    crossorigin="anonymous"
+  ></script>
+</head>
+
+<?php
+} ?>
--- sunriselogin.php
+++ sunriselogin.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sunrise login</title>
-    <link rel="stylesheet" href="CSS/styles.css" />
-  </head>
+  <?php include 'Includes/head.php';
+    head("sunrise login");
+  ?>
   <body>
     <section class="login">
       <div class="login-wrapper"></div>
     </section>
+    <?php include 'Includes/footer.php'; ?>
   </body>
 </html>
